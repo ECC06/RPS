98096042e17d589d491b8c5ee8c0aacc7716d034
Ethan Obott <217574989+ECC06@users.noreply.github.com>
2026-03-17 18:11:43 +0000

    let game keep running until user or computer has 3 wins

--- index.js
+++ index.js
@@ -1,76 +1,58 @@
+const array = ["rock", "paper", "scissors"]; //arr.includes("harrison") => false
+const randomNum = Math.random() * 3 // a random number between 0 and 2.99999999
+const roundedDown = Math.floor(randomNum) //round down the random number e.g 2.99999 rounded down to 2
 
-let validRound = 1;
-let choices = ["rock", "paper", "scissors"];
-let computerWins = 0;
 let userWins = 0;
-let exitIfGameOver = false;
+let computerWins = 0;
 
-while (validRound <= 5 && !exitIfGameOver) {
-    console.log(validRound);
-
-    const userChoice = prompt(
-        "ROCK! PAPER! SCISSORS! SHOOT! (Type in your response)",
-    ).toLowerCase();
-
-    const randomIndex = Math.floor(Math.random() * 3); //Generate a number between 0 & 2 inclusive
-    const computerChoice = choices[randomIndex];
-
-    const computerWinsWithPaper =
-        computerChoice === "paper" && userChoice === "rock";
-    const computerWinsWithScissors =
-        computerChoice === "scissors" && userChoice === "paper";
-    const computerWinsWithRock =
-        computerChoice === "rock" && userChoice === "scissors";
-
-    if (!choices.includes(userChoice)) {
-        alert("Invalid choice, try again");
-    }
-    else if (userChoice === computerChoice) {
-        alert(`Draw. Play again \nComputer score: ${computerWins} \nUser score: ${userWins}`);
-
-    } else if (computerWinsWithPaper || computerWinsWithScissors || computerWinsWithRock) {
-        validRound++;
-        computerWins++;
-        checkIfComputerWinsGame();
-    } else {
-        validRound++;
-        userWins++;
-        checkIfUserWinsGame();
-    }
-
-    function checkIfComputerWinsGame() {
-        if (computerWins === 3) {
+const randomIndex = array[roundedDown];
 
 
-            exitIfGameOver = true;
 
-            const gameOverForUser = `GAME OVER! YOU LOST 😞 \nComputer chose ${computerChoice} \nYou chose ${userChoice} \n\nComputer score: ${computerWins} \nUser score: ${userWins}`;
-
-            alert(gameOverForUser);
-
-        } else {
-            const computerWonRound = `Computer chose ${computerChoice} and you chose ${userChoice}. \nSo, you lose this round. 😔 \n\nComputer score: ${computerWins} \nUser score: ${userWins}`;
-
-            alert(computerWonRound);
+while (true) {
+    function stopGame() {
+        if (userWins === 3 || computerWins === 3) {
+            return true;
         }
     }
 
-
-    function checkIfUserWinsGame() {
-        if (userWins === 3) {
-            exitIfGameOver = true;
-
-            const gameOverForComputer = `GAME OVER! YOU WON! 🎉🎉🎉 \nComputer chose ${computerChoice} while you... \nchose ${userChoice}😎 \n\nComputer score: ${computerWins} \nUser score: ${userWins}`;
-
-            alert(gameOverForComputer);
-
-        } else {
-            const userWonString = `You chose ${userChoice} and computer chose ${computerChoice}. So, you win this round! 🎉 \n\nComputer score: ${computerWins} \nUser score: ${userWins} `;
-
-            alert(userWonString);
-        }
+    if (stopGame()) {
+        break;
     }
+
+    const userInput = prompt("ROCK! PAPER! SCISSORS! SHOOT!").toLowerCase();
+
+    function findWinner(userChoice, computerChoice) {
+
+        // (true && false) => false
+        const userWinsWithRock = userChoice === "rock" && computerChoice === "scissors";
+
+        //(false && true) => false
+        const userWinsWithScissors = userChoice === "scissors" && computerChoice === "paper";
+
+        //(false && false) => false
+        const userWinsWithPaper = userChoice === "paper" && computerChoice === "rock";
+
+
+        if (!array.includes(userChoice)) {
+            alert("Invalid input, please try again");
+        } else if (userChoice === computerChoice) {
+            alert(`Draw\nUser Points: ${userWins}\nComputer Points: ${computerWins}`);
+        } else if (userWinsWithRock || userWinsWithPaper || userWinsWithScissors) {
+            userWins++;
+            alert(`Your answer was: ${userChoice}\nComputer's answer was: ${computerChoice}\nYou win 🥳\n\nUser Points: ${userWins}\nComputer Points: ${computerWins}`);
+        } else {
+            computerWins++;
+            alert(`Computer's answer was: ${computerChoice}\nYour answer was: ${userChoice}\nYou lose 😭\n\nUser Points: ${userWins}\nComputer Points: ${computerWins}`);
+        }
+
+    }
+
+    findWinner(userInput, randomIndex);
+
 
 
 }
+
+alert("Game done!");
 
